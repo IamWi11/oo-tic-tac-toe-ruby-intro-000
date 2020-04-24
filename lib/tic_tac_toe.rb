d430f392c16972fb9f875f16711ad0557d4f075d
William <william.l.munoz@gmail.com>
2020-04-24 19:18:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ def turn
 end
 
 def won?
-  WIN_COMBINATIONS.each {|win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
   index_0 = win_combo[0]
   index_1 = win_combo[1]
   index_2 = win_combo[2]
@@ -78,7 +78,7 @@ def won?
 elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
   return win_combo
 end
-}
+end
 return false
 end
 
