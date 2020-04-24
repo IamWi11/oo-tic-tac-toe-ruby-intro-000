77909fab0258dfea23f7dfbf41b2ed99a8c6575d
William <william.l.munoz@gmail.com>
2020-04-24 19:20:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,19 +65,19 @@ end
 
 def won?
   WIN_COMBINATIONS.each do |win_combo|
-  index_0 = win_combo[0]
-  index_1 = win_combo[1]
-  index_2 = win_combo[2]
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-  position_1 = @board[index_0]
-  position_2 = @board[index_1]
-  position_3 = @board[index_2]
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-  return win_combo
-elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-  return win_combo
-end
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
 end
 return false
 end
